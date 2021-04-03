001108559d5d79adbbe5256adcb0776805b50d98
Ruslan <rus@gmx.com>
2021-04-04 00:39:23 +0100

    Make map out of items

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const Accordion = ({ items }) => {
+  const renderItems = items.map((item) => {
+    return (
+      <div>
+        <div className="title actibe">
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
   return <h1>{items.length}</h1>;
 };
 
