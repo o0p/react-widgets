f73c616608f8be8fd8c4cc552963ef4a0c5ec4d7
Ruslan <rus@gmx.com>
2021-04-07 00:54:39 +0100

    Went back to first axios search approach and add wiki api url

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,9 +5,18 @@ const Search = () => {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    (async () => {
-      await axios.get();
-    })();
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+    search();
   }, [term]);
 
   return (
