618dc79416034e1d248dbb7c49f27937c20822e4
Ruslan <rus@gmx.com>
2021-04-10 00:34:12 +0100

    Add options map and extarct value

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Dropdown = () => {
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return <div key={option.value}>{option.label}</div>;
+  });
   return <h1>Dropdown</h1>;
 };
 
