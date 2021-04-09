cb9b76c03356edecfb1940b0c28293e830d78943
Ruslan <rus@gmx.com>
2021-04-10 00:35:20 +0100

    Add options item

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Dropdown = ({ options }) => {
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>{option.label}</div>;
+    return (
+      <div key={option.value} className="item">
+        {option.label}
+      </div>
+    );
   });
   return <h1>Dropdown</h1>;
 };
